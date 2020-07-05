97c36c6546243463bb12ce30b39ae41994c23b6b
Josh Weinstein <jwdrexel92@gmail.com>
2020-07-05 16:41:06 -0400

    Updates

---
+++ precache-manifest.055ff4147f32d594886310c6de3bf0ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f4635c314f9657315eef6fca44355e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9d5404a095990e5c4c4",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "7acc5c2d3af093e9b350",
+    "url": "/static/css/main.9c23d48e.chunk.css"
+  },
+  {
+    "revision": "a9d5404a095990e5c4c4",
+    "url": "/static/js/2.36de3a2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36de3a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7acc5c2d3af093e9b350",
+    "url": "/static/js/main.5192c4bb.chunk.js"
+  },
+  {
+    "revision": "a623cd753d407dbd34f4",
+    "url": "/static/js/runtime-main.d78dac87.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
